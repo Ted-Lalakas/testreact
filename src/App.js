def1d4b9193dedb77c0dc05ecaf0bc7926d292c9
Ted Lalakas (DJCS) <Ted.Lalakas@justice.vic.gov.au>
2020-02-21 15:36:16 +1100

    Added a simple form and error validation for testing functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+state = {
+  questions: [
+    { question1: 'Is this a JOTE form', answer1: true, answer2: false },
+    { question2: 'Should I move to the next question?', answer1: true, answer2: false },
+    { question3: 'End of form', answer1: true, answer2: false }
+  ],
+  testingVar: "default text",
+  errorMessage: false
+}
+
+updateInput1 = (event) => {
+  // console.log('Was clicked!');
+  const matches = event.target.value.match(/\d+/g);
+  if (matches !== null) {
+    this.setState( {
+      errorMessage: true,
+      testingVar: ""
+    })
+  } else {
+    this.setState( {
+      errorMessage: false,
+      testingVar: event.target.value
+    })
+  }
+}
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <content>
+        <div className="input1-wrapper">
+          <label className="title">This is a test form (no numbers allowed)</label>
+
+          {this.state.errorMessage ? 
+          (
+          <div className="error-message">NO numbers allowed in this form!</div>
+          ) : null }
+
+          <input type="text" value={this.state.testingVar} onChange={this.updateInput1} />
+        </div>
+      </content>
+      <p className="content-value">Content Entered: <span>{this.state.testingVar}</span></p>
     </div>
   );
+ }
 }
 
 export default App;
